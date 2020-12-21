6408488ad926d218ea524524c85f9a5b971a3676
Kevin Alvarado <kevin.alvarado8502@utc.edu.ec>
2020-12-21 14:38:33 -0500

    master page was configurated and routing files modified with few pasth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DefaultComponent } from './public/home/default/default.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //son objetos que tienen atributos path
+  
+  { 
+    path : 'home',
+    component: DefaultComponent
+  },
+
+  { 
+    path : '',
+    pathMatch: 'full',
+    redirectTo: '/home'
+
+  },
+  // la ultima ruta es el comodin
+  {
+    path : '**',
+    redirectTo: '/home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/modules/security/security-routing.module.ts
+++ src/app/modules/security/security-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ChangePasswordComponent } from './change-password/change-password.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { PasswordResetComponent } from './password-reset/password-reset.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'logout',
+    component: LogoutComponent
+  },
+  {
+    path: 'reset',
+    component: PasswordResetComponent
+  },
+  {
+    path: 'change-password',
+    component: ChangePasswordComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
